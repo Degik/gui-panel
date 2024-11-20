af44ba4ab06f82fe61eab381f0b4e7e07181ab5f
Degik <prodax9900@gmail.com>
2024-11-20 18:04:32 +0100

    Fixed notification

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { useLocation, Routes, Route, Navigate } from 'react-router-dom';
 import classNames from 'classnames'; // For conditional Tailwind classes
-import {NotificationProvider, useNotification} from "./Notification/Notification";
+import { NotificationProvider, useNotification } from './Notification/Notification';
 import ChatAssistant from './ChatAssistant/ChatAssistant';
 import DashboardSidebar from './Sidebar/Sidebar';
 import Header from './Header/Header';
 import UserSettings from './UserSettings/UserSettings';
 import DataView from './DataView/DataView';
 import Home from './Home/Home';
-import LogPage from "./LogPage/LogPage";
+import LogPage from './LogPage/LogPage';
 
 interface DashboardProps {
     username: string;
@@ -16,7 +16,7 @@ interface DashboardProps {
     userAvatar?: string;
 }
 
-const SmartFactoryDashboard: React.FC<DashboardProps> = ({ username, role, userAvatar }) => {
+const SmartFactory: React.FC<DashboardProps> = ({ username, role, userAvatar }) => {
     const location = useLocation();
     const sectionMap: { [key: string]: string } = {
         home: 'Home',
@@ -32,7 +32,7 @@ const SmartFactoryDashboard: React.FC<DashboardProps> = ({ username, role, userA
 
     const section = sectionMap[location.pathname.replace('/', '')] || 'Unknown Section';
 
-    {/*TEST NOTIFICATION*/}
+    // TEST NOTIFICATION
     const { addNotification } = useNotification();
     const handleNewNotification = () => {
         addNotification({
@@ -46,14 +46,7 @@ const SmartFactoryDashboard: React.FC<DashboardProps> = ({ username, role, userA
     return (
         <div className="flex min-h-screen bg-gray-100">
             {/* Sidebar */}
-            <DashboardSidebar/>
-
-            {/*TEST NOTIFICATION*/}
-            <div>
-                <h1>Dashboard</h1>
-                <button onClick={handleNewNotification}>Add Test Notification</button>
-            </div>
-
+            <DashboardSidebar />
 
             {/* Main Content */}
             <main className="flex flex-col flex-grow">
@@ -66,25 +59,34 @@ const SmartFactoryDashboard: React.FC<DashboardProps> = ({ username, role, userA
                     role={role}
                 />
 
-                {/*Notification system*/}
-                <NotificationProvider>
-                    <div className="flex-grow p-4">
-                        {/* Dynamic Routes */}
-                        <Routes>
-                            <Route path="/" element={<Navigate to="home" replace/>}/>
-                            <Route path="home" element={<Home/>}/>
-                            <Route path="user-settings" element={<UserSettings/>}/>
-                            <Route path="data-view" element={<DataView/>}/>
-                            <Route path="log" element={<LogPage/>}/>
-                        </Routes>
-                    </div>
-                </NotificationProvider>
+                <div className="flex-grow p-4">
+                    {/* Dynamic Routes */}
+                    <Routes>
+                        <Route path="/" element={<Navigate to="home" replace />} />
+                        <Route path="home" element={<Home />} />
+                        <Route path="user-settings" element={<UserSettings />} />
+                        <Route path="data-view" element={<DataView />} />
+                        <Route path="log" element={<LogPage />} />
+                    </Routes>
+                </div>
             </main>
 
             {/* Chat Assistant */}
-            <ChatAssistant/>
+            <ChatAssistant />
+
+            {/* TEST NOTIFICATION */}
+            <div>
+                <h1>Dashboard</h1>
+                <button onClick={handleNewNotification}>Add Test Notification</button>
+            </div>
         </div>
     );
 };
+
+const SmartFactoryDashboard: React.FC<DashboardProps> = (props) => (
+    <NotificationProvider>
+        <SmartFactory {...props} />
+    </NotificationProvider>
+);
 
 export default SmartFactoryDashboard;
