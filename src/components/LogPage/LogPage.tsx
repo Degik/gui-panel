f7149c07ae73bff04716cd84e0dad476303b5ef0
Degik <prodax9900@gmail.com>
2024-11-20 17:33:47 +0100

    Updated LogPage

--- src/components/LogPage/LogPage.tsx
+++ src/components/LogPage/LogPage.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
-import styles from './styles/LogPage.module.css';
+import styles from '../styles/LogPage.module.css';
 import { fetchLogs, markAsRead } from '../../api/LogService';
+import { FaInfoCircle, FaExclamationTriangle, FaTimesCircle } from 'react-icons/fa';
 
 interface LogItem {
     id: number;
@@ -26,6 +27,19 @@ const LogPage: React.FC = () => {
         }
     };
 
+    const getIcon = (type: string) => {
+        switch (type) {
+            case 'Info':
+                return <FaInfoCircle className={styles.iconInfo} />;
+            case 'Warning':
+                return <FaExclamationTriangle className={styles.iconWarning} />;
+            case 'Error':
+                return <FaTimesCircle className={styles.iconError} />;
+            default:
+                return null;
+        }
+    };
+
     return (
         <div className={styles.logPage}>
             <h1 className={styles.title}>Log Notifications</h1>
@@ -34,11 +48,16 @@ const LogPage: React.FC = () => {
                     <li
                         key={log.id}
                         className={`${styles.logItem} ${log.isRead ? styles.read : styles.unread}`}
-                        onClick={() => handleExpand(log.id)}
                     >
-                        <div className={styles.logHeader}>
-                            <span className={styles.logType}>{log.type}</span>
-                            <span className={styles.logHeaderText}>{log.header}</span>
+                        <div className={styles.logHeader} onClick={() => handleExpand(log.id)}>
+                            <div className={styles.iconContainer}>{getIcon(log.type)}</div>
+                            <div className={styles.textContainer}>
+                                <span className={styles.logHeaderText}>{log.header}</span>
+                                <span className={styles.logType}>{log.type}</span>
+                            </div>
+                            <span className={styles.expandToggle}>
+                {expandedLog === log.id ? '-' : '+'}
+              </span>
                         </div>
                         {expandedLog === log.id && (
                             <div className={styles.logBody}>
